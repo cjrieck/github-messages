4459cb661bca0c460c78cd4b7927a6e6c00a9ec8
Clayton R <cjrieck123@gmail.com>
2014-06-25 08:21:29 -0400

    adding static files

--- web.js
+++ web.js
@@ -1,8 +1,20 @@
 var express = require("express");
 var logfmt = require("logfmt");
+var Handlebars = require("handlebars");
+var exphbs = require('express3-handlebars')
+var firebase = require("firebase");
+var _ = require('lodash')
+var bodyParser = require("body-parser");
 var app = express();
 
 app.use(logfmt.requestLogger());
+app.use(bodyParser.json());
+
+app.engine('.hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}));
+app.set('view engine', '.hbs');
+
+app.use("/", express.static(__dirname+"/public"));
+// add other things to serve here
 
 app.get("/", function(req, res){
 	// Render template
