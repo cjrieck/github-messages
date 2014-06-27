731ca2a2e45bcf30743b68211527e7270ed00bd2
Clayton R <cjrieck123@gmail.com>
2014-06-27 09:39:09 -0400

    make initial github request

--- public/js/main.js
+++ public/js/main.js
@@ -4,4 +4,20 @@ $(function(){
 
 	$(".container").height(windowHeight);
 	$(".container").width(windowWidth);
+
+	function initialize() 
+	{
+		$.ajax({
+			type: "GET",
+			url: "/members",
+			success: function(data) {
+				$(".member-names").html(data);
+			},
+			error: function(jqXHR, textStatus, errorThrown){
+				console.log("bad: " + textStatus + ": " + errorThrown);
+			}
+		});
+	}
+
+	initialize();
 });
--- web.js
+++ web.js
@@ -2,10 +2,17 @@ var express = require("express");
 var logfmt = require("logfmt");
 var Handlebars = require("handlebars");
 var exphbs = require('express3-handlebars')
-var firebase = require("firebase");
+var Firebase = require("firebase");
 var _ = require('lodash')
 var bodyParser = require("body-parser");
+var GitHubApi = require("github");
 var app = express();
+
+var firebaseRoot = new Firebase("https://github-messages.firebaseio.com/");
+var github = new GitHubApi({
+    // required
+    version: "3.0.0",
+});
 
 app.use(logfmt.requestLogger());
 app.use(bodyParser.json());
@@ -16,8 +23,27 @@ app.set('view engine', '.hbs');
 app.use(express.static(__dirname+"/public"));
 
 app.get("/", function(req, res){
-	var context = {};
-	res.render("body", context);
+	// var RZMembers = /orgs/:org/members
+	var context = {};//{members: };
+	github.orgs.getMembers({org: "Raizlabs"}, function(err, items){
+		if (err) {
+			res.send(err);
+		}
+
+		var context = {members: items};
+		res.render("body", context);
+	});
+});
+
+app.get("/members", function(req, res){
+	github.orgs.getMembers({org: "Raizlabs"}, function(err, items){
+		if (err) {
+			res.send(err);
+		}
+
+		var context = {members: items};
+		res.render("members", _.extend(context, {layout: false}));
+	});
 });
 
 var port = Number(process.env.PORT || 5000);
